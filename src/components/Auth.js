5a610de57e49cb17cc57980f9e6c75047174e747
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-03-14 23:43:59 -0400

    moved all components into a components folder, and sub-folder of forum inside that.

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,6 +1,6 @@
-import MagicLink from './MagicLink'
-import SignUp from './SignUp'
-import PasswordSignIn from './PasswordSignIn'
+import MagicLink from './forum/MagicLink'
+import SignUp from './forum/SignUp'
+import PasswordSignIn from './forum/PasswordSignIn'
 
 export default function Auth() {
      return (
